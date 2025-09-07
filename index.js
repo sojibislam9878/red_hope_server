92a7baef660a8dcdc00c99ff8520fade1982e6cd
sojibislam9878 <sojibhasan9878@gmail.com>
2025-09-07 22:00:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("This is template server");
+});
+
+app.listen(port, () => {
+  console.log(`This server is runing on port no: ${port}`);
+});
