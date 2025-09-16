77926eb7496694b36eb27fa0e173734d7b99a74c
sojibislam9878 <sojibhasan9878@gmail.com>
2025-09-16 19:10:43 +0600

    signup and loging routes created

--- index.js
+++ index.js
@@ -1,9 +1,98 @@
 const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
 const app = express();
 const port = process.env.PORT || 3000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+// middleware
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lb51cqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+    const userCollection = client.db("Red-Hope-DB").collection("allUsers");
+
+    app.get("/allUser", async (req, res) => {
+      try {
+        const cursor = userCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Error fetching users");
+      }
+    });
+
+    // auth related
+    app.post("/signup", async (req, res) => {
+      console.log(req.body);
+      const { firstName, lastName, email, password, phone, terms } = req.body;
+      const existingUser = await userCollection.findOne({ email });
+      if (existingUser) return res.status(400).send("Email already exists");
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await userCollection.insertOne({
+        firstName,
+        lastName,
+        email,
+        phone,
+        password: hashedPassword,
+        terms,
+        role: "user",
+        createdAt: new Date(),
+      });
+
+      res.send("Signup successful");
+    });
+
+    app.post("/login", async (req, res) => {
+      try {
+        const { email, password } = req.body;
+        const user = await userCollection.findOne({ email });
+        if (!user) return res.status(400).send("Invalid email");
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).send("Invalid password");
+        const { password: _, ...userWithoutPassword } = user;
+        res.status(200).json({
+          success: true,
+          message: "Login successful",
+          user: userWithoutPassword,
+        });
+      } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+      }
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("This is template server");
+  res.send("hello world is template server");
 });
 
 app.listen(port, () => {
